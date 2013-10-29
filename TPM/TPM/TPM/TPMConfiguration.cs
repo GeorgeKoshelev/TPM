6baf68f53a346c6a3560d7cf1f63c3e406e07a83
george.koshelev <george.koshelev@gmail.com>
2013-10-30 00:09:26 +0600

    Add configuration file

---
+++ TPM/TPM/TPM/TPMConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace TPM.TPM
+{
+    public class TPMConfiguration
+    {
+        public int InputNeuronCount { get; set; }
+        public int HiddenNeuronCount { get; set; }
+        public int WeightRange { get; set; }
+    }
+}
