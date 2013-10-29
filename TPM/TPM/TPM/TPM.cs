ba3df7e878791998af2ef5d97ce180f549f6c320
george.koshelev <george.koshelev@gmail.com>
2013-10-30 00:25:55 +0600

    add TPM class

---
+++ TPM/TPM/TPM/TPM.cs
@@ -0,0 +1,13 @@
+﻿namespace TPM.TPM
+{
+    public class TPM
+    {
+        private readonly TPMConfiguration configuration;
+
+        public TPM(TPMConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+    }
+}
