0021bcb19df7b4bdef540784dae730a2205f8db3
george.koshelev <george.koshelev@gmail.com>
2013-10-30 00:12:38 +0600

    add constructor , override ToString method

--- TPM/TPM/TPM/TPMConfiguration.cs
+++ TPM/TPM/TPM/TPMConfiguration.cs
@@ -1,9 +1,24 @@
-﻿namespace TPM.TPM
+﻿using System;
+
+namespace TPM.TPM
 {
     public class TPMConfiguration
     {
-        public int InputNeuronCount { get; set; }
-        public int HiddenNeuronCount { get; set; }
-        public int WeightRange { get; set; }
+        public TPMConfiguration(int inputNeuronCount, int hiddenNeuronCount, int weightRange)
+        {
+            InputNeuronCount = inputNeuronCount;
+            HiddenNeuronCount = hiddenNeuronCount;
+            WeightRange = weightRange;
+        }
+
+        public int InputNeuronCount { get; private set; }
+        public int HiddenNeuronCount { get; private set; }
+        public int WeightRange { get; private set; }
+
+        public override string ToString()
+        {
+            return String.Format("InputNeuronCount:{0},HiddenNeuronCount:{1},WeightRange:{2}", InputNeuronCount,
+                                 HiddenNeuronCount, WeightRange);
+        }
     }
 }
