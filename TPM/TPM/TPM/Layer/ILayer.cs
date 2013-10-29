2a4ced766daa111522e5de6979e6a0a3aec04527
george.koshelev <george.koshelev@gmail.com>
2013-10-30 01:54:00 +0600

    add layer class and interface

---
+++ TPM/TPM/TPM/Layer/ILayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TPM.TPM.Neuron;
+
+namespace TPM.TPM.Layer
+{
+    public interface ILayer
+    {
+        List<INeuron> Neurons { get; }
+
+        INeuron this[int index] { get; }
+
+        void Run();
+        int GetOutput();
+    }
+}
---
+++ TPM/TPM/TPM/Layer/Layer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using TPM.TPM.Neuron;
+using System.Linq;
+
+namespace TPM.TPM.Layer
+{
+    public class Layer : ILayer
+    {
+        public Layer(List<INeuron> neurons )
+        {
+            Neurons = neurons;
+        }
+
+        public List<INeuron> Neurons { get; private set; }
+
+        public INeuron this[int index]
+        {
+            get { return Neurons[index]; }
+        }
+
+        public void Run()
+        {
+            Neurons.ForEach(x=> x.Run());
+        }
+
+        public int GetOutput()
+        {
+            return Neurons.Sum(x => x.Output);
+        }
+    }
+}
