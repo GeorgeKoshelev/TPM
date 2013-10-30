14a5a8d8900a940b587f101f5221c1bf53c3f8f8
george.koshelev <george.koshelev@gmail.com>
2013-10-30 19:46:25 +0600

    add example of sync two networks

--- TPM/TPM/Program.cs
+++ TPM/TPM/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace TPM
+﻿using System.Collections.Generic;
+using TPM.TPM;
+
+namespace TPM
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var configuration = new TPMConfiguration(12, 8, 4);
+            var tpm1 = TPMFactory.GetInstance(configuration);
+            var tpm2 = TPMFactory.GetInstance(configuration);
+
+            var tutor = new Tutor(configuration);
+            tutor.SyncTPM(tpm1 , tpm2);
+
         }
     }
 }
